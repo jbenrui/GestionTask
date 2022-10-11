f8fc56d6e6aafc6b259dc72f55309aa1ab5a6739
JoseAntonioBenitez <benitezruiz.joseantonio@gmail.com>
2022-10-11 20:29:35 +0200

    Version 2 del proyecto

--- src/app/pages/persons/persons.page.ts
+++ src/app/pages/persons/persons.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { People } from 'src/app/models/person.model';
+import { DataPersonService } from 'src/app/services/dataperson.service';
 
 @Component({
   selector: 'app-persons',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./persons.page.scss'],
 })
 export class PersonsPage implements OnInit {
-
-  constructor() { }
+  public person: People[]; //Declaro el servicio y lo instancio en la clase home
+  constructor(
+    private dataPerson:DataPersonService, //Instancio esta variable la cual contendra los datos de la persona.
+    private activatedRoute : ActivatedRoute //preguntar para que sirve
+  ) { }
 
   ngOnInit() {
   }
 
+  getPeople(){ //Creo el metodo para devolver personas
+    return this.dataPerson.getPersons(); //Llamo al metodo de la clase DataPersonService y lo devuelvo
+  }
 }
---
+++ src/app/services/dataperson.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { People} from '../models/person.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataPersonService {
+  person:People[] = [
+    {
+      id: 0,
+      nombre: 'Jose Antonio',
+      apodo:  'Hijo mayor',
+      foto: 'http://drive.google.com/uc?export=view&id=1dTD1aemHNG7hfjztTxEyXFvp4hZsPD5j'
+    },
+    {
+      id: 1,
+      nombre: 'Francisco Javier',
+      apodo:  'Hijo menor',
+      foto: 'http://drive.google.com/uc?export=view&id=1JUp0ES_V5WyMSNvD1qjB_yc7n6_3pvEW'
+    },
+    {
+      id: 2,
+      nombre: 'Jose Antonio',
+      apodo:  'Padre',
+      foto: 'http://drive.google.com/uc?export=view&id=1MlO1YViatOBIpIaqM0rPGMcNjoQWvRmb'
+    },
+    {
+      id: 3,
+      nombre: 'Maria Dolores',
+      apodo:  'Madre',
+      foto: 'http://drive.google.com/uc?export=view&id=1qJlC0am4aM0KdHcafo3vZSdMAV2s2B_S'
+    }
+  ]
+  constructor() { }
+
+  public getPersons(): People[]{ //devuelve el objeto persona del array
+    return this.person;
+  }
+  
+  public getPersonsById(id:number): People{
+    return this.person[id];
+  }
+}
